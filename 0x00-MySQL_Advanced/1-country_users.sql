35a8c15d1a64b5afb1b5cd4225aa154e238fcaa4
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-21 23:07:53 +0000

    Solution to task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- A script that creates a table user.
 
-CREATE TABLE IF NOT EXISTS user(
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
