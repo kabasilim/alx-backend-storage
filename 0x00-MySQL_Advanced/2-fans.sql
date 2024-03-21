d61d029c14875ec9a651ed25395b5d80a588b05e
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-21 23:11:21 +0000

    solution to task 2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- This sql script queries the metal_bands table and ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
