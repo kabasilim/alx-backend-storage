85b15965dd88ba9e9115e72432b299bfafdb28f3
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-21 22:57:50 +0000

    Solution to task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- A script that creates a new table called users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
