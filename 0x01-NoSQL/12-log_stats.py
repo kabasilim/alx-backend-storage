5a5b08a60d63d71a28705ac36ac1e59da0abf38d
AAbasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-26 16:15:25 -0700

    Solution to task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
-"""This script provides statistics about Nginx logs stored in MongoDB"""
-
+"""This file provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
-def main():
-    # Connect to MongoDB
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    nginx_collection = db.nginx
-
-    # Count total logs
-    total_logs = nginx_collection.count_documents({})
-
-    # Count logs by method
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {}
-    for method in methods:
-        method_counts[method] = nginx_collection.count_documents({"method": method})
-
-    # Count status check
-    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-
-    # Display statistics
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check} status check")
 
 if __name__ == "__main__":
-    main()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
+    count = nginx_collection.count_documents({})
+    get_count = nginx_collection.count_documents({'method': 'GET'})
+    post_count = nginx_collection.count_documents(
+        {'method': 'POST'})
+    put_count = nginx_collection.count_documents(
+        {'method': 'PUT'})
+    patch_count = nginx_collection.count_documents(
+        {'method': 'PATCH'})
+    delete_count = nginx_collection.count_documents(
+        {'method': 'DELETE'})
+    status_check = nginx_collection.count_documents({"path": "/status"})
+
+    print(f"{count} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get_count}")
+    print(f"\tmethod POST: {post_count}")
+    print(f"\tmethod PUT: {put_count}")
+    print(f"\tmethod PATCH: {patch_count}")
+    print(f"\tmethod DELETE: {delete_count}")
+    print(f"{status_check} status check")
